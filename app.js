08ea3f7e0579d97fb20e25fc329936498a6affe0
JP <>
2018-01-12 15:13:06 +0100

    add PORT to app.js

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server has started.");
 });
